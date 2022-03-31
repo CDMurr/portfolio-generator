4c18fe025a99fe575c9e156a6fe028fbf0004371
Cody Murray <cdillon.murray@yahoo.com>
2022-03-31 11:30:58 -0500

    added new page-template.js file

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This... 
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
+
+
+
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
+// const fs = require('fs');
+
+// const generatePage = (userName, githubName) => {
+//     return `
+//    <!DOCTYPE html>
+//    <html lang="en">
+//    <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Portfolio Demo</title>
+//     <head> 
+    
+//     <body>
+//         <h1>${name}</h1>
+//         <h2><a href="https://github.com/${github}">Github</a></h2>
+//         </body>
+//         </html>
+//    `;
+// };
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+// const printProfileData = profileDataArr => {
+//     // This... 
+//     for (let i = 0; i < profileDataArr.length; i+= 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
