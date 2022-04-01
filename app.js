c7ec97986ffcf5c52808744b5f0fc6cbdf252b0f
Cody Murray <cdillon.murray@yahoo.com>
2022-04-01 12:37:29 -0500

    expanded HTML template to account for more profile data

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const inquirer = require('inquirer');
+
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -130,15 +134,19 @@ Add a New Project
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+       if(err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in the directory to see it!');
+     });
 });
 
 
 
 
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
